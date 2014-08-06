856587c114fc0ad0a761a862dd6bd8ab833dfbcc
Valentin Bryukhanov <bvv.mag@gmail.com>
2014-08-06 15:45:38 +1200

    referee

--- verification/referee.py
+++ verification/referee.py
@@ -1,31 +1,3 @@
-"""
-CheckiOReferee is a base referee for checking you code.
-    arguments:
-        tests -- the dict contains tests in the specific structure.
-            You can find an example in tests.py.
-        cover_code -- is a wrapper for the user function and additional operations before give data
-            in the user function. You can use some predefined codes from checkio.referee.cover_codes
-        checker -- is replacement for the default checking of an user function result. If given, then
-            instead simple "==" will be using the checker function which return tuple with result
-            (false or true) and some additional info (some message).
-            You can use some predefined codes from checkio.referee.checkers
-        add_allowed_modules -- additional module which will be allowed for your task.
-        add_close_builtins -- some closed builtin words, as example, if you want, you can close "eval"
-        remove_allowed_modules -- close standard library modules, as example "math"
-
-checkio.referee.checkers
-    checkers.float_comparison -- Checking function fabric for check result with float numbers.
-        Syntax: checkers.float_comparison(digits) -- where "digits" is a quantity of significant
-            digits after coma.
-
-checkio.referee.cover_codes
-    cover_codes.unwrap_args -- Your "input" from test can be given as a list. if you want unwrap this
-        before user function calling, then using this function. For example: if your test's input
-        is [2, 2] and you use this cover_code, then user function will be called as checkio(2, 2)
-    cover_codes.unwrap_kwargs -- the same as unwrap_kwargs, but unwrap dict.
-
-"""
-
 from checkio.signals import ON_CONNECT
 from checkio import api
 from checkio.referees.io import CheckiOReferee
@@ -34,16 +6,49 @@ from checkio.referees import checkers
 
 from tests import TESTS
 
+cover = """def cover(func, data):
+    cdata = set(tuple(d) for d in data)
+    res = func(data)
+    if not isinstance(res, (tuple, list)):
+        raise TypeError("Must be a list or a tuple.")
+    return res
+"""
+
+
+def checker(data, user_result):
+    for t in user_result:
+        if not isinstance(t, tuple) or len(t) != 2 or not isinstance(t[0], int) or not isinstance(t[1], int):
+            return False, (False, "You should return a list/tuple of tuples with two integers in each.")
+    if not data:
+        if user_result:
+            return False, (True, "How did you draw this?")
+        else:
+            return True, (True, "Great")
+    if len(user_result) < 2:
+        return False, (False, "Only one point in your result.")
+    data = list(data)
+    for i in range(len(user_result) - 1):
+        f, s = user_result[i], user_result[i + 1]
+        if (f + s) in data:
+            data.remove(f + s)
+        elif (s + f) in data:
+            data.remove(s + f)
+        else:
+            return False, (True, "The wrong segment {}.".format(f + s))
+    if data:
+        return False, (True, "You forgot about {}.".format(data[0]))
+    return True, (True, "Great")
+
+
 api.add_listener(
     ON_CONNECT,
     CheckiOReferee(
         tests=TESTS,
         cover_code={
-            'python-27': cover_codes.unwrap_args,  # or None
-            'python-3': cover_codes.unwrap_args
+            'python-27': cover,  # or None
+            'python-3': cover
         },
-        # checker=None,  # checkers.float.comparison(2)
-        # add_allowed_modules=[],
-        # add_close_builtins=[],
-        # remove_allowed_modules=[]
+        checker=checker,
+        function_name="draw",
+
     ).on_ready)
